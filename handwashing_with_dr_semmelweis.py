ae6a8451a27dd6cebcfc629e2f7b9352b122015d
Python Tell Me <63752153+pythontellme@users.noreply.github.com>
2020-04-29 09:08:24 +0000

    Add files via upload

--- handwashing_with_dr_semmelweis.py
+++ handwashing_with_dr_semmelweis.py
@@ -1,103 +1,115 @@
+"""
+A script for scraping and cleaning two websites for data which is
+used for a comparative analysis.
+"""
+
 import pandas as pd
 import scrape_wikipedia_for_table as scrape
 import matplotlib.pyplot as plt
 
 ''' Scrape and clean yearly data from Wikipedia '''
 
+# Call function to scrape website and get html
 website = scrape.scraper('https://en.wikipedia.org/wiki/Ignaz_Semmelweis')
 
-scraped_list = website.scrape('wikitable', 'tr')   
+# Find 'wikitable' class and 'tr' tag in html
+scraped_list = website.scrape('wikitable', 'tr')
 
+# Put table data into dataframe
 wrangled_list = scrape.wrangle_list(scraped_list)
 
+# Remove trailing '/na' characters from dataframe elements
 scrape.remove_line_breaks(wrangled_list)
 
+# Remove ',' characters from elements in 'Births' column
 scrape.remove_comma(wrangled_list['Births'])
 
+# Change datatype of 'Year' column to date
 wrangled_list['Year'] = wrangled_list['Year'].apply(pd.to_datetime)
 
-wrangled_list[['Births', 'Deaths']] = wrangled_list[['Births', 'Deaths']].apply(pd.to_numeric)   
+# Change datatype of 'Births' and 'Deaths' columns to numeric
+wrangled_list[['Births', 'Deaths']] = wrangled_list[['Births', 'Deaths']].apply(pd.to_numeric)
 
-# print(wrangled_list)
+''' Scrape and clean monthly data from Wikipedia '''
 
-''' Scrape and clean yearly data from Wikipedia '''
-
+# Call function to scrape website and get html
 website2 = scrape.scraper('https://en.wikipedia.org/wiki/Historical_mortality_rates_of_puerperal_fever')
 
+# Find 'wikitable sortable' class and 'tr' tag in html
 scraped_list2 = website2.scrape('wikitable sortable', 'tr')
 
+# Put table data into dataframe
 wrangled_list2 = scrape.wrangle_list2(scraped_list2)
 
+# Remove trailing '/na' characters from dataframe elements
 scrape.remove_line_breaks(wrangled_list2)
 
+# Remove row with missing data
 wrangled_list2 = wrangled_list2[wrangled_list2['Births']!='na']
 
+# Change datatype of 'Year' column to date 
 wrangled_list2['Year'] = wrangled_list2['Year'].apply(pd.to_datetime)
 
+# Change datatype of 'Births' and 'Deaths' columns to numeric
 wrangled_list2[['Births', 'Deaths']] = wrangled_list2[['Births', 'Deaths']].apply(pd.to_numeric)   
 
+# Reset index of dataframe
 wrangled_list2.reset_index(drop=True, inplace = True)
 
-# print(wrangled_list2)
+# ''' Analyze yearly data '''
 
-''' Analyze data '''
+# Calculate mortality rate
+wrangled_list['Mortality_Rate'] = wrangled_list['Deaths']/wrangled_list['Births']
+
+# Put clinic 1 data and clinic 2 into seperate variables
+yearly1 = wrangled_list[wrangled_list['Clinic']=='First']
+yearly2 = wrangled_list[wrangled_list['Clinic']=='Second']
+
+# Plot yearly mortality rates at the two clinics
+plot1 = yearly1.plot(x='Year', y='Mortality_Rate', label='Clinic 1')
+plot2 = yearly2.plot(x='Year', y='Mortality_Rate', label='Clinic 2', ax=plot1)
+plot2.set_ylabel('Mortality Rate (%)')
+
+''' Analyze monthly data '''
 
 # Calculate proportion of deaths per no. births
+wrangled_list2["Mortality_Rate"] = wrangled_list2["Deaths"]/wrangled_list2["Births"]
 
-yearly = wrangled_list
-
-yearly['proportion_deaths'] = yearly['Deaths']/yearly['Births']
-
-# Extract clinic 1 data into yearly1 and clinic 2 data into yearly2
-yearly1 = yearly[yearly['Clinic']=='First']
-yearly2 = yearly[yearly['Clinic']=='Second']
-
-# Print out yearly1
-# print(yearly1)
-
-# Plot yearly proportion of Deaths at the two clinics
-plot1 = yearly1.plot(x='Year', y='proportion_deaths', label='yearly1')
-plot2 = yearly2.plot(x='Year', y='proportion_deaths', label='yearly2', ax=plot1)
-plot2.set_ylabel('% deaths')
-# plt.show()
-
-monthly = wrangled_list2
-# Calculate proportion of deaths per no. births
-monthly["proportion_deaths"] = monthly["Deaths"]/monthly["Births"]
-
-# Print out the first rows in monthly
-# print(monthly.head())
-
-# Plot monthly proportion of deaths
-ax = monthly.plot(x='Year', y='proportion_deaths')
-# plt.show()
-
-# Date when handwashing was made mandatory
+# Put date when handwashing was made mandatory into variable
 handwashing_start = pd.to_datetime('1847-06-01')
 
-# Split monthly into before and after handwashing_start
-before_washing = monthly[monthly["Year"]<handwashing_start]
-after_washing = monthly[monthly["Year"]>=handwashing_start]
+# Split monthly data into before and after handwashing
+before_washing = wrangled_list2[wrangled_list2["Year"]<handwashing_start]
+after_washing = wrangled_list2[wrangled_list2["Year"]>=handwashing_start]
 
-# Plot monthly proportion of deaths before and after handwashing
-ax = before_washing.plot(x='Year', y='proportion_deaths', label='clinic1') 
-after_washing.plot(x='Year', y='proportion_deaths', label='clinic2', ax=ax)
-ax.set_ylabel("Proportion deaths")
-# plt.show()
+# Plot monthly mortality rate before and after handwashing
+ax = before_washing.plot(x='Year', y='Mortality_Rate', label='Before handwashing') 
+after_washing.plot(x='Year', y='Mortality_Rate', label='After handwashing', ax=ax)
+ax.set_ylabel("Mortality Rate (%)")
+plt.show()
 
-# Difference in mean monthly proportion of deaths due to handwashing
-before_proportion =before_washing["proportion_deaths"]
-after_proportion =after_washing["proportion_deaths"]
+# Find average mortility rate before and after
+# handwashing and compute difference
+before_proportion =before_washing["Mortality_Rate"]
+after_proportion =after_washing["Mortality_Rate"]
 mean_diff = after_proportion.mean()-before_proportion.mean()
-print(mean_diff)
 
-# A bootstrap analysis of the reduction of deaths due to handwashing
-boot_mean_diff = []
+print(f'before hand washing: {before_proportion.mean()*100:.1f} percent')
+print(f'after hand washing: {after_proportion.mean()*100:.1f} percent')
+print(f'difference: {mean_diff*100:.1f} percent')
+
+# Create a sample distribution of the reduction 
+# in mortality rates due to handwashing
+mean_diff = []
 for i in range(3000):
-    boot_before = before_proportion.sample(frac=1, replace=True)
-    boot_after = after_proportion.sample(frac=1, replace=True)
-    boot_mean_diff.append(boot_after.mean() - boot_before.mean())
+    before = before_proportion.sample(frac=1, replace=True)
+    after = after_proportion.sample(frac=1, replace=True)
+    mean_diff.append(after.mean() - before.mean())
 
-# Calculating a 95% confidence interval from boot_mean_diff 
-confidence_interval = pd.Series(boot_mean_diff).quantile([0.025, 0.975])
-print(confidence_interval)
+# Calculating a 95% confidence interval from mean_diff 
+confidence_interval = pd.Series(mean_diff).quantile([0.025, 0.975])   
+print(f'95% confidence interval is between {confidence_interval[0.975]*100:.1f}%\
+ and {confidence_interval[0.025]*100:.1f}%')
+
+
+
--- scrape_wikipedia_for_table.py
+++ scrape_wikipedia_for_table.py
@@ -1,37 +1,105 @@
+"""
+This module contains classes and functions
+for scraping and cleaning tables from specific
+websites.
+"""
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 class scraper:
+    """
+    Used to create an object containing the
+    html in a table on a website
+
+    ...
+
+    Attributes
+    ----------
+    url : str
+        The website url we're scraping
+
+    Methods
+    -------
+    scrape (class_name, tag)
+        Returns the html within a specific class with
+        a specific tag on the website
+    """
 
     def __init__(self, url):
+        """
+        Parameters
+        ----------
+        url: str
+            The website url we're scraping
+        """
         self.url = url
 
     def scrape(self, class_name, tag):
-        website_url = requests.get(self.url).text      
+        """
+        Returns the html within a specific class with
+        a specific tag on the website
+
+        Parameters
+        ----------
+        
+        class_name : str
+            The html class of the table we're scraping
+
+        tag : str
+            The html tag of the table we're scraping
+        """
+        # Use requests library to get the html
+        # of the given url and assign it to variable
+        website_url = requests.get(self.url).text
+        # Parse through html and create a tree
         soup = BeautifulSoup(website_url,'lxml')
+        # Find a subset of html with table tag in
+        # a given class
         my_table = soup.find('table',{'class':class_name})
+        # Find html in table within given tag
         table_rows = my_table.findAll(tag)
         return table_rows
 
 def wrangle_list (list):
+    """
+    Returns a pandas dataframe that has rows and columns
+    that reflect the html table we're scraping
+
+    Parameters
+    ----------
+    list : bs4.element.ResultSet
+        A beautiful soup object containing html
+    """
     
+    # Create a pandas dataframe that will mirror the html table
     df = pd.DataFrame(columns = ['Year', 'Births', 'Deaths', 'Clinic'])
+    
+    # Create 4 lists to hold html for each column in table
     Year = []
     Births = []
     Deaths = []
     Clinic = []
 
+    # Loop through items in beaufiful soup object w html
+    # and add to relevant list - skip first two 'rows' since
+    # they are headers in table 
     for i in range(2,8):
             Year.append(list[i].findAll('td')[0].string)
             Births.append(list[i].findAll('td')[1].string)
             Deaths.append(list[i].findAll('td')[2].string)
+            # The items appended so far are data for 
+            # first clinic
             Clinic.append('First')
             Year.append(list[i].findAll('td')[0].string)
             Births.append(list[i].findAll('td')[5].string)
             Deaths.append(list[i].findAll('td')[6].string)
+            # The 3 previous items are data for the
+            # second clinic
             Clinic.append('Second')
 
+    # Create columns in dataframe using data in lists
     df['Year'] = Year
     df['Births'] = Births
     df['Deaths'] = Deaths
@@ -40,20 +108,40 @@ def wrangle_list (list):
     return df
 
 def wrangle_list2 (list):
-    
+    """
+    Returns a pandas dataframe that has rows and columns
+    that reflect the html table we're scraping
+
+    Parameters
+    ----------
+    list : bs4.element.ResultSet
+        A beautiful soup object containing html
+    """
+    # Create a pandas dataframe that will mirror the html table
     df = pd.DataFrame(columns = ['Year', 'Births', 'Deaths'])
+
+    # Create 3 lists to hold html for each column in table
     Year = []
     Births = []
     Deaths = []
 
+    # Loop through items in beaufiful soup object w html
+    # and add to relevant list - skip first 'row' since
+    # it contains headers in table 
     for i in range(1,len(list)):
+            # The 'year' string we need to extract is within a 'span'
+            # tag in lines 1-24 
             if (i<=24):
                 Year.append(list[i].findAll('span')[0].string)
+            # The 'year' string is in within a 'td' tag in 
+            # other rows 
             else:
                 Year.append(list[i].findAll('td')[1].string)
+
             Births.append(list[i].findAll('td')[2].string)
             Deaths.append(list[i].findAll('td')[3].string)
                     
+    # Create columns in dataframe using data in lists
     df['Year'] = Year
     df['Births'] = Births
     df['Deaths'] = Deaths
@@ -61,47 +149,30 @@ def wrangle_list2 (list):
     return df
 
 def remove_line_breaks (df):
+    """
+    Loop through each item in dataframe and 
+    apply strip function which removes leading and 
+    trailing characters
+
+    Parameters
+    ----------
+    df : pandas dataframe
+        The dataframe we're cleaning
+    """
     for i in df.columns:
         for j in range(len(df)):
              df.loc[j,i] = df.loc[j,i].strip()
 
 def remove_comma (column):
+    """
+    Loop through each row item in dataframe column
+    and remove the ',' character
+
+    Parameters
+    ----------
+    column : pandas series
+        A specific column of the dataframe we're cleaning
+    """
     for x in range(len(column)):
         column[x]  = column[x].replace(',','')
     return column
-
-# ''' Scrape yearly data '''
-
-# website = scraper('https://en.wikipedia.org/wiki/Ignaz_Semmelweis')
-
-# scraped_list = website.scrape('wikitable', 'tr')   
-
-# wrangled_list = wrangle_list(scraped_list)
-
-# remove_line_breaks(wrangled_list)
-
-# remove_comma(wrangled_list['Births'])
-
-# wrangled_list['Year'] = wrangled_list['Year'].apply(pd.to_datetime)
-
-# wrangled_list[['Births', 'Deaths']] = wrangled_list[['Births', 'Deaths']].apply(pd.to_numeric)   
-
-# ''' Scrape monthly data '''
-
-# website2 = scraper('https://en.wikipedia.org/wiki/Historical_mortality_rates_of_puerperal_fever')
-
-# scraped_list2 = website2.scrape('wikitable sortable', 'tr')
-
-# wrangled_list2 = wrangle_list2(scraped_list2)
-
-# remove_line_breaks(wrangled_list2)
-
-# wrangled_list2 = wrangled_list2[wrangled_list2['Births']!='na']
-
-# wrangled_list2['Year'] = wrangled_list2['Year'].apply(pd.to_datetime)
-
-# wrangled_list2[['Births', 'Deaths']] = wrangled_list2[['Births', 'Deaths']].apply(pd.to_numeric)   
-
-# wrangled_list2.reset_index(drop=True, inplace = True)
-
-# print(wrangled_list2)
